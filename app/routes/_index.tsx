f574bea4d33a8c78b94fbb3c39cf77ddb215534d
Lasse Kløvstad <lasse.klovstad@ciber.no>
2023-05-23 17:25:34 +0200

    Legg til favicon og midlertidig forside tekst

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,17 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "Lasse & Sonica" }];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: "system-ui, sans-serif" }}>
+      <h1>Heisann!</h1>
+      <p style={{ lineHeight: "1.0" }}>
+        Du er litt tidlig ute. Her kommer snart informasjon om bryllupet til
+        Lasse & Sonica.
+      </p>
     </div>
   );
 }
