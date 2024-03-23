1cba0636ac705cf62230475f4504108a4ef5f818
Lasse Kløvstad <lasse.klovstad@ciber.no>
2024-03-23 14:13:24 +0100

    Upgrade cloudflare

--- app/utils/siteSecret.tsx
+++ app/utils/siteSecret.tsx
@@ -5,12 +5,12 @@ export type AccessLevel = "fullAccess" | "limitedAccess";
 
 export const validateSecret = (secret: string, context: AppLoadContext) => {
   const fullAccess =
-    secret === (context.env.LOGIN_SECRET_FULL ?? "full access");
+    secret === (context.cloudflare.env.LOGIN_SECRET_FULL ?? "full access");
   if (fullAccess) {
     return "fullAccess";
   }
   const limitedAccess =
-    secret === (context.env.LOGIN_SECRET_LIMITED ?? "limited access");
+    secret === (context.cloudflare.env.LOGIN_SECRET_LIMITED ?? "limited access");
   if (limitedAccess) {
     return "limitedAccess";
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-import { unstable_vitePlugin as remix, unstable_cloudflarePreset as cloudflare, } from "@remix-run/dev";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin,
+} from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [
-        remix({
-        ignoredRouteFiles: ["**/.*"],
-        presets: [cloudflare()],
-    }),
-    tsconfigPaths()
-    ],
-    ssr: {
-        resolve: {
-          externalConditions: ["workerd", "worker"],
-        },
-      },
+  plugins: [cloudflareDevProxyVitePlugin(), remix(), tsconfigPaths()],
+  ssr: {
+    resolve: {
+      externalConditions: ["workerd", "worker"],
+    },
+  },
 });
