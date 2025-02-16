22358044208e8c37c4f727af311b18f61295acfc
Lasse Narula <lasse.klovstad@gmail.com>
2025-02-16 16:53:52 +0100

    Jalla

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    cloudflareDevProxy({
-      getLoadContext({ context }) {
-        return { cloudflare: context.cloudflare };
-      },
-    }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [cloudflareDevProxy(), reactRouter(), tsconfigPaths()],
 });
