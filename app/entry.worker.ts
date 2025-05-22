a7582944c8af5d79ed66be4680f00a4199476113
Lasse Narula <lasse.klovstad@gmail.com>
2025-05-22 19:35:39 +0200

    Fix env type issue

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -3,14 +3,18 @@ import { createRequestHandler } from "react-router";
 // eslint-disable-next-line import/no-unresolved
 import * as build from "virtual:react-router/server-build";
 
-import { CloudflareContext } from "./middleware/bindings";
+import { CloudflareContext, EnvSchema } from "./middleware/bindings";
 
 const handler = createRequestHandler(build);
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
     const context: unstable_InitialContext = new Map();
-    context.set(CloudflareContext, { env, ctx, cf: request.cf });
+    context.set(CloudflareContext, {
+      env: EnvSchema.safeParse(env),
+      ctx,
+      cf: request.cf,
+    });
     return await handler(request, context);
   },
 };
--- app/middleware/bindings.ts
+++ app/middleware/bindings.ts
@@ -1,7 +1,17 @@
 import { unstable_createContext } from "react-router";
+import { z } from "zod";
+
+export const EnvSchema = z.object({
+  LOGIN_SECRET_FULL: z.string(),
+  LOGIN_SECRET_LIMITED: z.string(),
+  LOGIN_SECRET_ADMIN: z.string(),
+  CLOUDINARY_NAME: z.string(),
+  RESEND_API_KEY: z.string(),
+  DB: z.instanceof(D1Database),
+});
 
 export const CloudflareContext = unstable_createContext<{
-  env: Env;
+  env: z.infer<typeof EnvSchema>;
   ctx: ExecutionContext;
   cf?: RequestInitCfProperties;
 }>();
